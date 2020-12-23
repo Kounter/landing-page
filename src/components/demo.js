09539706fa1a1fedffb96a6cfad7e42be047e970
meetzaveri <meethpatel96@gmail.com>
2020-12-23 16:23:15 +0530

    update: update demo section

--- src/components/demo.js
+++ src/components/demo.js
@@ -3,16 +3,22 @@ import BlueLineSVG from 'assets/blue_line.svg';
 
 const DemoSection = (props) => {
   return (
-    <div className="py-20">
-      <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-          <img
-            class="object-cover object-center rounded"
-            alt="hero"
-            src="https://dummyimage.com/720x400"
-          />
+    <div id="demo-section-container" className="py-20">
+      <div class="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
+        <div class="lg:max-w-lg lg:w-1/2 md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <div class="video-iframe">
+            <iframe
+              className="w-full h-64"
+              //   width="711"
+              //   height="400"
+              src="https://www.youtube.com/embed/cmofCLP8zKg"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
         </div>
-        <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+        <div class="lg:flex-grow md:w-1/2 lg:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <div>
             <img src={BlueLineSVG} />
           </div>
